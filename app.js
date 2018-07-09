66874285e16949833a636010be653317eb727b8d
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-07-09 15:55:24 -0600

    added cors middleware

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require('cors')
 
 const fosterSearch = require('./routes/foster-search')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 
 app.use('/foster-search', fosterSearch)
 
