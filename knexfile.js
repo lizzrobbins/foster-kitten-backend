28360406e2028e1e1bd62e0eb20db59bc34e6c30
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-07-09 13:58:31 -0600

    ready for deployment to Heroku

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,14 @@
-// Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
   development: {
     client: 'postgresql',
     connection: 'postgres://localhost/kittens-development'
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
   }
 
 };
